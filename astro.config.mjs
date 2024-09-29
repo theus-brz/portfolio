f1f7746c0335c49e339dafa86c926c4b7148bf45
Matheus Barbosa <matheus.brbs@gmail.com>
2024-09-29 18:14:41 -0300

    fixes stylesheet not being found

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://theus-brz.github.io/',
-  base: '/theus-brz.github.io',
+  base: '',
   integrations: [tailwind()],
 });
