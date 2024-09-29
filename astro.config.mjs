8aad8c31bdc9639719d3b902852c788557557e45
Matheus Barbosa <matheus.brbs@gmail.com>
2024-09-29 18:07:15 -0300

    adds github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://theus-brz.github.io/',
+  base: '/theus-brz.github.io',
   integrations: [tailwind()],
 });
